d13b67245c0ac066c6030b7dec19dc5156aae810
KosmosLock <cos.lo.iac@gmail.com>
2021-11-04 01:03:07 +0100

    form login component added

--- Netflix/src/app/app.module.ts
+++ Netflix/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainHomepageComponent } from './homepage/main-homepage/main-homepage.component';
 import { MainLoginComponent } from './login/main-login/main-login.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainHomepageComponent,
-    MainLoginComponent
+    MainLoginComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
