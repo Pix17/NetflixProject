b34ff974d70ae06e8e533bcdd53ab3d48bd5ea82
Pix17 <giuseppecapicotto7@gmail.com>
2021-11-09 17:29:05 +0100

    add service component

--- Netflix/src/app/app.module.ts
+++ Netflix/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainHomepageComponent } from './homepage/main-homepage/main-homepage.component';
 import { MainLoginComponent } from './login/main-login/main-login.component';
+import { ServiceComponent } from './homepage/service/service.component';
+import { ServiceContentComponent } from './homepage/service-content/service-content.component';
+import { ServiceMediaComponent } from './homepage/service-media/service-media.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainHomepageComponent,
-    MainLoginComponent
+    MainLoginComponent,
+    ServiceComponent,
+    ServiceContentComponent,
+    ServiceMediaComponent
   ],
   imports: [
     BrowserModule,
