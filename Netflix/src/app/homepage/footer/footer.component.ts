bcb8f84358e1f90d23a4e7ccc1eacd999284593c
Riccardo Mammina <riccardo.mammina@gmail.com>
2021-11-10 19:50:38 +0100

    aggiunto footer in homepage

--- Netflix/src/app/homepage/footer/footer.component.ts
+++ Netflix/src/app/homepage/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ILink } from 'src/app/models/footer-interface';
 
 @Component({
   selector: 'footer',
@@ -6,6 +7,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+
+column1: ILink[] = [
+  {
+    label: 'Domande frequenti',
+    url: '#'
+  },
+  {
+    label: 'Rapporti con gli investitori',
+    url: '#'
+  },
+  {
+    label: 'Come guardare Netflix',
+    url: '#'
+  },
+  {
+    label: `Informazioni sull'azienda`,
+    url: '#'
+  },
+  {
+    label: 'Solo su Netflix',
+    url: '#'
+  }
+];
+
+column2: ILink[] = [
+  {
+    label: 'Centro assistenza',
+    url: '#'
+  },
+  {
+    label: 'Opportunità di lavoro',
+    url: '#'
+  },
+  {
+    label: 'Condizioni di utilizzo',
+    url: '#'
+  },
+  {
+    label: `Contattaci`,
+    url: '#'
+  }
+];
+
+column3: ILink[] = [
+  {
+    label: 'Account',
+    url: '#'
+  },
+  {
+    label: 'Riscatta carte regalo',
+    url: '#'
+  },
+  {
+    label: 'Privacy',
+    url: '#'
+  },
+  {
+    label: `Test di velocità`,
+    url: '#'
+  }
+];
+
+column4: ILink[] = [
+  {
+    label: 'Media Center',
+    url: '#'
+  },
+  {
+    label: 'Acquista carte regalo',
+    url: '#'
+  },
+  {
+    label: 'Preferenze per i cookie',
+    url: '#'
+  },
+  {
+    label: `Note legali`,
+    url: '#'
+  }
+]
 
   constructor() { }
 
