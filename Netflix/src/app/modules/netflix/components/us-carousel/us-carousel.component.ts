9005a81cd627e0a685b1af70b8a3bba04f44b39d
Greta9626 <86754253+Greta9626@users.noreply.github.com>
2022-01-14 03:34:29 +0100

    fix: browser page

--- Netflix/src/app/modules/netflix/components/us-carousel/us-carousel.component.ts
+++ Netflix/src/app/modules/netflix/components/us-carousel/us-carousel.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  faChevronLeft,
+  faChevronRight,
+  faPlay,
+  faPlus,
+  faThumbsDown,
+  faThumbsUp,
+} from '@fortawesome/free-solid-svg-icons';
 import { ApiCallService } from '../../services/api-call.service';
 
 @Component({
@@ -8,11 +16,32 @@ import { ApiCallService } from '../../services/api-call.service';
 })
 export class UsCarouselComponent implements OnInit {
   constructor(private readonly api: ApiCallService) {}
-  
-@Input() list:any;
-@Input() name!:string;
 
+  @ViewChild('row') row: any;
 
-  ngOnInit(): void {
+  @Input() list: any;
+  @Input() name!: string;
+
+  faPlay = faPlay;
+  faPlus = faPlus;
+  faThumbsUp = faThumbsUp;
+  faThumbsDown = faThumbsDown;
+  faChevronLeft = faChevronLeft;
+  faChevronRight = faChevronRight;
+
+  ngOnInit(): void {}
+
+  public scrollRight(): void {
+    this.row.nativeElement.scrollTo({
+      left: this.row.nativeElement.scrollLeft + 800,
+      behavior: 'smooth',
+    });
+  }
+
+  public scrollLeft(): void {
+    this.row.nativeElement.scrollTo({
+      left: this.row.nativeElement.scrollLeft - 800,
+      behavior: 'smooth',
+    });
   }
 }
